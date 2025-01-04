3e42ceea33704830dbed7fe6791f26015e9f1f17
Ricardo Leite <me@ricleite.pt>
2025-01-04 15:42:58 +0100

    Add day 7

---
+++ src/day_07p1/prog.rs
@@ -0,0 +1,83 @@
+/*
+--- Day 7: Bridge Repair ---
+
+The Historians take you to a familiar rope bridge over a river in the middle of a jungle. The Chief isn't on this side of the bridge, though; maybe he's on the other side?
+
+When you go to cross the bridge, you notice a group of engineers trying to repair it. (Apparently, it breaks pretty frequently.) You won't be able to cross until it's fixed.
+
+You ask how long it'll take; the engineers tell you that it only needs final calibrations, but some young elephants were playing nearby and stole all the operators from their calibration equations! They could finish the calibrations if only someone could determine which test values could possibly be produced by placing any combination of operators into their calibration equations (your puzzle input).
+
+For example:
+
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+
+Each line represents a single equation. The test value appears before the colon on each line; it is your job to determine whether the remaining numbers can be combined with operators to produce the test value.
+
+Operators are always evaluated left-to-right, not according to precedence rules. Furthermore, numbers in the equations cannot be rearranged. Glancing into the jungle, you can see elephants holding two different types of operators: add (+) and multiply (*).
+
+Only three of the above equations can be made true by inserting operators:
+
+    190: 10 19 has only one position that accepts an operator: between 10 and 19. Choosing + would give 29, but choosing * would give the test value (10 * 19 = 190).
+    3267: 81 40 27 has two positions for operators. Of the four possible configurations of the operators, two cause the right side to match the test value: 81 + 40 * 27 and 81 * 40 + 27 both equal 3267 (when evaluated left-to-right)!
+    292: 11 6 16 20 can be solved in exactly one way: 11 + 6 * 16 + 20.
+
+The engineers just need the total calibration result, which is the sum of the test values from just the equations that could possibly be true. In the above example, the sum of the test values for the three equations listed above is 3749.
+
+Determine which equations could possibly be true. What is their total calibration result?
+*/
+
+use std::env;
+use std::fs::read_to_string;
+
+fn resolve(left: usize, v : &[usize]) -> Vec<usize> {
+    // NOTE: left-to-right evaluation
+    if v.is_empty() {
+        return vec![left];
+    }
+
+    let mut res1 = resolve(left + v.first().unwrap(), &v[1..v.len()]);
+    let mut res2 = resolve(left * v.first().unwrap(), &v[1..v.len()]);
+    let mut res: Vec<usize> = Vec::new();
+    res.append(&mut res1);
+    res.append(&mut res2);
+    return res;
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let input = read_to_string(filename).unwrap();
+
+    let mut equations: Vec<Vec<usize>> = Vec::new();
+    for line in input.lines() {
+        let mut v: Vec<usize> = Vec::new();
+        let mut itr = line.split(": ");
+        v.push(itr.next().unwrap().parse::<usize>().unwrap());
+        for s in itr.next().unwrap().split(" ") {
+            v.push(s.parse::<usize>().unwrap());
+        }
+
+        equations.push(v);
+    }
+
+    let mut sum = 0;
+    for eq in equations {
+        let target = eq[0];
+        let left = eq[1];
+        let res = resolve(left, &eq[2..eq.len()]);
+
+        if res.contains(&target) {
+            sum += target;
+        }
+    }
+
+    println!("{}", sum);
+}
